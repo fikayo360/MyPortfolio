5836d8f86e3cebe29333c52aef82879fb2071cda
fikayo <fikayoadele@gmail.com>
2023-10-03 00:04:27 +0100

    about

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,13 +1,42 @@
 import './appStyles.css'
+import { useState,useRef } from 'react'
+import emailjs from '@emailjs/browser';
 
 function Contact(){
+    const [success,setSucess] = useState(false)
+    const [notification,setNot] = useState('')
+    const form = useRef();
+    const clear = () => {
+        setTimeout(()=>{
+            setNot('')
+        },5000)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        emailjs.sendForm('service_rmhtq3i', 'template_xbmg67d', form.current, '-s6cbcgwAZ_RR88P3')
+        .then((result) => {
+            console.log(result.text);
+            setSucess(!success)
+            setNot('email sent successfully')
+            form.current.reset();
+            clear()
+
+        }, (error) => {
+            console.log(error.text);
+            setSucess(false)
+        });
+    }
+
 return (
     <div id='contact'>
         <h1 id='contactMainTxt'>contact me</h1>
         <div id = 'contactWrap'>
             <div id='contactLeft'>
-                <textarea id='input' name="comment" rows="10" cols="30" placeholder="send @fikayo a message"></textarea>
+            <form id='wrapForm' ref={form} onSubmit={handleSubmit}>
+            <textarea id='input' name="message" rows="10" cols="30" placeholder="send @fikayo a message"></textarea>
             <button id='submit'>submit</button>
+            <p id='not'> {notification} </p>
+            </form>
 
             <div id='socialIcons'>
             <div className='socialIcon'>
